02edd62d59b8436e8afb65fdee495256cdf04b44
milton2913 <milton2913@gmail.com>
2021-01-18 18:37:31 +0600

    User Management Complete

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\UsersController;
+use App\Http\Controllers\Admin\RolesController;
+use App\Http\Controllers\Admin\PermissionsController;
+use App\Http\Controllers\Auth\ChangePasswordController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +19,47 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//Route::get('/get-bup-course', [GetBupCourseController::class, 'index']);
+
+
+Route::get('/home', function () {
+    if (session('status')) {
+        return redirect()->route('admin.home')->with('status', session('status'));
+    }
+
+    return redirect()->route('admin.home');
+});
+
+
+Auth::routes(['register' => false]);
+// Admin
+Route::resources([
+    'admin/users' => UsersController::class,
+
+]);
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth']], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+
+    Route::get('home', [HomeController::class, 'index'])->name('home');
+
+
+    Route::resources([
+        'permissions' => PermissionsController::class,
+        'roles' => RolesController::class,
+        'users' => UsersController::class,
+    ]);
+    Route::get('/', [HomeController::class, 'index']);
+    Route::delete('permissions/destroy', [PermissionsController::class, 'massDestroy'])->name('permissions.massDestroy');
+    Route::delete('roles/destroy', [RolesController::class, 'massDestroy'])->name('roles.massDestroy');
+    Route::delete('users/destroy', [UsersController::class, 'massDestroy'])->name('users.massDestroy');
+
+});
+Route::group(['prefix' => 'profile', 'as' => 'profile.', 'namespace' => 'Auth', 'middleware' => ['auth']], function () {
+// Change password
+    if (file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php'))) {
+        Route::get('password', [ChangePasswordController::class,'edit'])->name('password.edit');
+        Route::post('password', [ChangePasswordController::class,'update'])->name('password.update');
+    }
 });
